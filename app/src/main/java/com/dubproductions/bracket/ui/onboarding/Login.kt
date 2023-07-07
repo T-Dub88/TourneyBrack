2731ed12ccd0fbd30729361f63a19873d925a9e7
Tyler Williams <tylerlw1988@gmail.com>
2023-07-07 16:03:55 -0400

    Add navigation and icons to login screen

--- app/src/main/java/com/dubproductions/bracket/ui/onboarding/Login.kt
+++ app/src/main/java/com/dubproductions/bracket/ui/onboarding/Login.kt
@@ -3,56 +3,82 @@ package com.dubproductions.bracket.ui.onboarding
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.dubproductions.bracket.R
+import com.dubproductions.bracket.navigation.Screen
 import com.dubproductions.bracket.viewmodel.UserViewModel
 
 @Composable
 fun LoginScreen(userViewModel: UserViewModel, navHostController: NavHostController) {
 
-    var emailText by remember { mutableStateOf("") }
-    var passwordText by remember { mutableStateOf("") }
+    var emailText by rememberSaveable { mutableStateOf("") }
+    var passwordText by rememberSaveable { mutableStateOf("") }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
 
-    Column(
-
-    ) {
+    Column {
         Text(text = "Login")
-        LoginScreenTextField(
+
+        // Text field for email
+        OnboardingTextField(
             text = emailText,
             whenChanged = { newText -> emailText = newText },
             hint = "Email",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 25.dp
-                )
+            leadingIcon = Icons.Filled.Email,
+            trailingIcon = Icons.Filled.Clear,
+            trailingIconOnClick = { emailText = "" },
+            visualTransformation = VisualTransformation.None
         )
-        LoginScreenTextField(
+
+        // Text field for password
+        OnboardingTextField(
             text = passwordText,
             whenChanged = { newText -> passwordText = newText },
             hint = "Password",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    horizontal = 25.dp
-                )
+            leadingIcon = Icons.Filled.Lock,
+            trailingIcon = if (passwordVisible) {
+                Icons.Filled.Visibility
+            } else {
+                Icons.Filled.VisibilityOff
+            },
+            trailingIconOnClick = { passwordVisible = !passwordVisible },
+            visualTransformation = if (passwordVisible) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            }
         )
+
+        // Button to log user in
         LoginScreenButton(
             whenClicked = { /*TODO*/ },
             buttonText = "Login"
         )
+
+        // Button to navigate to the registration screen
         LoginScreenButton(
-            whenClicked = { /*TODO*/ },
+            whenClicked = { navHostController.navigate(Screen.Registration.route) },
             buttonText = "Registration"
         )
     }
@@ -61,17 +87,31 @@ fun LoginScreen(userViewModel: UserViewModel, navHostController: NavHostControll
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LoginScreenTextField(
+fun OnboardingTextField(
     text: String,
     whenChanged: (String) -> Unit,
     hint: String,
-    modifier: Modifier
+    leadingIcon: ImageVector,
+    trailingIcon: ImageVector,
+    trailingIconOnClick: () -> Unit,
+    visualTransformation: VisualTransformation
 ) {
    OutlinedTextField(
        value = text,
        onValueChange = whenChanged,
        label = { Text(text = hint) },
-       modifier = modifier
+       leadingIcon = { Icon(imageVector = leadingIcon, contentDescription = null) },
+       trailingIcon = {
+           IconButton(onClick = trailingIconOnClick) {
+               Icon(imageVector = trailingIcon, contentDescription = null)
+           }
+       },
+       modifier = Modifier
+           .fillMaxWidth()
+           .padding(
+               horizontal = 25.dp
+           ),
+       visualTransformation = visualTransformation
    )
 }
 
