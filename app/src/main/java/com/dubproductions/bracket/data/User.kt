49dd28adf35439e8dffc8739e9a505f87f892ade
Tyler Williams <tylerlw1988@gmail.com>
2023-07-07 12:50:23 -0400

    Add firebase login and registration code

--- app/src/main/java/com/dubproductions/bracket/data/User.kt
+++ app/src/main/java/com/dubproductions/bracket/data/User.kt
@@ -1,5 +1,9 @@
 package com.dubproductions.bracket.data
 
 data class User(
-    var userId: String? = null
+    var userId: String? = null,
+    var username: String? = null,
+    var firstName: String? = null,
+    var lastName: String? = null,
+    var email: String? = null
 )
---
+++ app/src/main/java/com/dubproductions/bracket/firebase/FirebaseManager.kt
@@ -0,0 +1,96 @@
+package com.dubproductions.bracket.firebase
+
+import android.util.Log
+import com.dubproductions.bracket.data.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+private const val TAG = "Firebase Manager"
+
+class FirebaseManager {
+
+    private val auth: FirebaseAuth = Firebase.auth
+    private val firestore: FirebaseFirestore = Firebase.firestore
+
+    fun registerUser(email: String, password: String, onComplete: (success: Boolean) -> Unit) {
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    task.result.user?.uid?.let { userId ->
+                        createUserData(
+                            userData = User(
+                                userId = userId,
+                                email = email,
+                            )
+                        ) { success ->
+                            if (!success) {
+                                deleteAuthUser()
+                            }
+                            onComplete(success)
+                        }
+                    }
+                } else {
+                    Log.e(TAG, "registerUser: ${task.exception}")
+                    onComplete(false)
+                }
+            }
+    }
+
+    private fun createUserData(userData: User, onComplete: (success: Boolean) -> Unit) {
+        userData.userId?.let { userId ->
+            firestore
+                .collection("Users")
+                .document(userId)
+                .set(userData)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        onComplete(true)
+                    } else {
+                        Log.e(TAG, "createUserData: ${task.exception}")
+                        onComplete(false)
+                    }
+                }
+        }
+    }
+
+    private fun deleteAuthUser() {
+        auth.currentUser?.delete()
+    }
+
+    fun signInUser(email: String, password: String, onComplete: (success: Boolean) -> Unit) {
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onComplete(true)
+                } else {
+                    Log.e(TAG, "signInUser: ${task.exception}")
+                    onComplete(false)
+                }
+            }
+    }
+
+    fun fetchUserData(onComplete: (user: User) -> Unit) {
+        auth.currentUser?.uid?.let { userId ->
+            firestore
+                .collection("Users")
+                .document(userId)
+                .addSnapshotListener { value, error ->
+                    if (error != null) {
+                        Log.e(TAG, "fetchUserData: ${error.message}")
+                        return@addSnapshotListener
+                    }
+                    if (value != null && value.exists()) {
+                        val user = value.toObject<User>()
+                        user?.let { onComplete(it) }
+                    }
+                }
+        }
+    }
+
+}
