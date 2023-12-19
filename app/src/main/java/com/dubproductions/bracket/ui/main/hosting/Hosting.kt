82dd729c5b58566450a112841f76b49dc1a6b9ec
Tyler Williams <tylerlw1988@gmail.com>
2023-12-19 16:39:01 -0500

    Create hosting screen and tournament creation screen

---
+++ app/src/main/java/com/dubproductions/bracket/ui/main/hosting/Hosting.kt
@@ -0,0 +1,80 @@
+package com.dubproductions.bracket.ui.main.hosting
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import com.dubproductions.bracket.R
+import com.dubproductions.bracket.data.Tournament
+import com.dubproductions.bracket.navigation.Screen
+import com.dubproductions.bracket.ui.TournamentSummaryCard
+import com.dubproductions.bracket.viewmodel.UserViewModel
+
+@Composable
+fun HostingScreen(
+    userViewModel: UserViewModel,
+    hostingNavHostController: NavHostController
+) {
+
+    val hostingTournamentList by userViewModel.hostingTournamentList.collectAsStateWithLifecycle()
+
+    HostingScreenContent(
+        hostingTournamentList = hostingTournamentList,
+        floatingActionButtonClick = {
+            hostingNavHostController.navigate(Screen.TournamentCreation.route)
+        },
+        tournamentCardClick = {
+            // Todo: Set action for clicking existing tournament
+        }
+    )
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HostingScreenContent(
+    hostingTournamentList: MutableList<Tournament>,
+    floatingActionButtonClick: () -> Unit,
+    tournamentCardClick: () -> Unit
+) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { floatingActionButtonClick() },
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = null
+                )
+            }
+        }
+    ) {
+        Column(
+            Modifier.padding(it)
+        ) {
+            Text(text = "Currently Hosting:")
+            LazyColumn() {
+                items(hostingTournamentList) { tournament ->
+                    TournamentSummaryCard(
+                        tournament = tournament,
+                        onPress = { tournamentCardClick() },
+                        tapCardInstruction = stringResource(id = R.string.update_tournament)
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dubproductions/bracket/ui/main/hosting/TournamentCreation.kt
@@ -0,0 +1,8 @@
+package com.dubproductions.bracket.ui.main.hosting
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TournamentCreationScreen() {
+
+}
