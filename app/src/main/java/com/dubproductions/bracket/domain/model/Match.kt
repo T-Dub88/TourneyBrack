9096831928fbb269e3f12b3945cc8deb53517f3a
Tyler Williams <tylerlw1988@gmail.com>
2024-02-02 17:00:27 -0500

    Change properties to var

--- app/src/main/java/com/dubproductions/bracket/domain/model/Match.kt
+++ app/src/main/java/com/dubproductions/bracket/domain/model/Match.kt
@@ -3,11 +3,11 @@ package com.dubproductions.bracket.domain.model
 import com.dubproductions.bracket.utils.status.MatchStatus
 
 data class Match(
-    val matchId: String? = null,
-    val playerOneId: String? = null,
-    val playerTwoId: String? = null,
+    var matchId: String? = null,
+    var playerOneId: String? = null,
+    var playerTwoId: String? = null,
     var winnerId: String? = null,
     var tie: Boolean? = null,
-    val round: Int? = null,
+    var round: Int? = null,
     var status: String? = null
 )
--- app/src/main/java/com/dubproductions/bracket/domain/model/Participant.kt
+++ app/src/main/java/com/dubproductions/bracket/domain/model/Participant.kt
@@ -1,10 +1,10 @@
 package com.dubproductions.bracket.domain.model
 
 data class Participant(
-    val username: String? = null,
-    val userId: String? = null,
+    var username: String? = null,
+    var userId: String? = null,
     var points: Double? = null,
-    val matchIds: List<String>? = null,
+    var matchIds: List<String>? = null,
     var buchholz: Double? = null,
     var sonnebornBerger: Double? = null,
     var dropped: Boolean? = null,
